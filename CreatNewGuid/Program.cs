079b000882b3db70aab5118816c81e12be048304
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-28 17:27:50 +0500

    Add project files.

---
+++ CreatNewGuid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TextCopy;
+
+namespace CreatNewGuid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            do
+            {
+                string stringFildGuid = Guid.NewGuid().ToString();
+                Console.WriteLine(stringFildGuid);
+                TextCopy.ClipboardService.SetText(stringFildGuid);
+
+                Console.WriteLine("do you want another Guid ? (yes or press any bottom to exit )");
+                userInput = Console.ReadLine();
+            }
+            while (userInput == "yes");
+
+        }
+    }
+}
+
