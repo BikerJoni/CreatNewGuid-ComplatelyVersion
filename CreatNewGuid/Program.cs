b7d870475786a0436567feee16553a785d852b4b
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-07-01 21:04:40 +0500

    Creat new Guid

--- CreatNewGuid/Program.cs
+++ CreatNewGuid/Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using TextCopy;
 
-namespace CreatNewGuid
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string userInput;
+        do
         {
-            string userInput;
-            do
-            {
-                string stringFildGuid = Guid.NewGuid().ToString();
-                Console.WriteLine(stringFildGuid);
-                TextCopy.ClipboardService.SetText(stringFildGuid);
+            string stringFildGuid = Guid.NewGuid().ToString();
+            Console.WriteLine(stringFildGuid);
+            string dateTimeOffSet = DateTimeOffset.UtcNow.ToString();
+            Console.WriteLine(dateTimeOffSet);
+            ClipboardService.SetText($" {stringFildGuid} {dateTimeOffSet}");
 
-                Console.WriteLine("do you want another Guid ? (yes or press any bottom to exit )");
-                userInput = Console.ReadLine();
-            }
-            while (userInput == "yes");
-
+            Console.WriteLine("do you want another Guid ? (if yes press + , or any bottom to exit )");
+            userInput = Console.ReadLine();
         }
+        while (userInput == "+");
     }
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CreatNewGuid
+### In order to creat .exe file, we need:
+1. Build project, by presing right side of mouse on the name of our solution.
+2. next we need to pres "Publish" by the same way on the top.
+
+#### If anybody can not creat .exe file, you can find instruction by this video:
+https://youtu.be/rMr3ejOEiDY?si=0fvC1ZxaEZbUHsxA
