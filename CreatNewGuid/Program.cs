ac79689e6a7a2912cc6e0a243f245c97c297642a
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-07-02 21:15:51 +0500

    final version

--- CreatNewGuid/Program.cs
+++ CreatNewGuid/Program.cs
@@ -1,22 +1,29 @@
 ﻿using System;
+using System.Reflection;
 using TextCopy;
 
-internal class Program
+string userInput;
+do
 {
-    private static void Main(string[] args)
+    
+    Console.WriteLine("Press 1 for new Guid or press 2 for DateTimeOffSet");
+    int input = int.Parse(s: Console.ReadLine());
+    if (input is 1)
     {
-        string userInput;
-        do
-        {
-            string stringFildGuid = Guid.NewGuid().ToString();
-            Console.WriteLine(stringFildGuid);
-            string dateTimeOffSet = DateTimeOffset.UtcNow.ToString();
-            Console.WriteLine(dateTimeOffSet);
-            ClipboardService.SetText($" {stringFildGuid} {dateTimeOffSet}");
-
-            Console.WriteLine("do you want another Guid ? (if yes press + , or any bottom to exit )");
-            userInput = Console.ReadLine();
-        }
-        while (userInput == "+");
+        string stringFildGuid = Guid.NewGuid().ToString();
+        Console.WriteLine(stringFildGuid);
+        ClipboardService.SetText(stringFildGuid);
+        Console.WriteLine("New Guid has been copied");
     }
+    else if (input is 2)
+    {
+        string dateTimeOffSet = DateTimeOffset.UtcNow.ToString();
+        Console.WriteLine(dateTimeOffSet);
+        ClipboardService.SetText(dateTimeOffSet);
+        Console.WriteLine("New DateTimeOffSet has been copied");
+    }
+
+    Console.WriteLine("do you want another Guid ? (if yes press + , or any bottom to exit )");
+    userInput = Console.ReadLine();
 }
+while (userInput == "+");
